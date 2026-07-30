c6b97e243e0d4aee7286281ddf9612df3a53327e
gillaashwitha <gillaashwitha@gmail.com>
2026-07-30 10:23:09 +0000

    added

---
+++ Inherit.java
@@ -0,0 +1,23 @@
+class Super 
+{
+    void show()
+{
+    System.out.println("i'm in csm a");
+}    
+}
+class sub extends Super
+{
+    void dispaly()
+    {
+        system.out.println("java class");
+    }
+}
+class Inherit
+{
+    public static void main(string args[])
+    {
+    sub obj1=new Sub();
+    obj1.show();
+    obj1.display();
+    }
+}
