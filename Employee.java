06d30867e94ee35d356062eca7f5049eb03f9de6
gillaashwitha <gillaashwitha@gmail.com>
2026-08-20 09:26:20 +0000

    ADDED

---
+++ Employee.java
@@ -0,0 +1,18 @@
+public class Employee
+{
+static int empid=500; static void emp1()
+{
+empid++;
+System.out.println("Employee id:"+empid);
+}
+}
+class Sample
+{
+public static void main(String args[])
+{
+Employee.emp1(); Employee.emp1(); Employee.emp1(); Employee.emp1(); Employee.emp1(); Employee.emp1();
+}
+}
+ 
+    
+
