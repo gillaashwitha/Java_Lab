9641318eb182e94913060577f6d35e06465e848a
gillaashwitha <gillaashwitha@gmail.com>
2026-08-27 08:48:13 +0000

    Added

---
+++ LogicalOp.java
@@ -0,0 +1,12 @@
+public class LogicalOp
+{
+public static void main(String[] args)
+{
+int x=10;
+System.out.println(x==10 && x>=5); 
+System.out.println(x==10 || x>=5); 
+System.out.println ( ! ( x==10 ));
+}
+} 
+    
+
